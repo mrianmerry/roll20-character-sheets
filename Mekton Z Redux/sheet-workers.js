4ebb6679fd1436124c428d93c22a8dbe1a6a535d
Ian Merryweather <mrianmerry@gmail.com>
2020-12-10 16:51:47 +0000

    feature: Adds character type selection
    - Styles configuration tab to use a grid layout
    - Adds character type select field
    - Removes need for sheet worker to set the theme by setting attribute name of input and selector to the same thing
    - Uses the same mechanism for character type selection
    - Renamed the Character tab to Pilot
    - Hides the Character / Mecha tab based on Pilot / Mecha character type selection

--- Mekton Z Redux/sheet-workers.js
+++ Mekton Z Redux/sheet-workers.js
@@ -1,14 +1,8 @@
-const tabNames = ["character", "combat", "configuration", "journal", "mecha"];
+const tabNames = ["combat", "configuration", "journal", "mecha", "pilot"];
 tabNames.forEach((button) => {
   on(`clicked:${button}`, function () {
     setAttrs({
       "selected-tab": button,
     });
-  });
-});
-
-on("change:theme", function (eventInfo) {
-  setAttrs({
-    "selected-theme": eventInfo.newValue,
   });
 });
